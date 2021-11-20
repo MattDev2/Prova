70f9d7e0493031aef339933ec0fe8623a5b1489a
Utente <Utente@PC-Matteo>
2021-11-20 16:58:36 +0100

    Commit Locale

--- Prova/src/prova/Prova.java
+++ Prova/src/prova/Prova.java
@@ -15,7 +15,9 @@ public class Prova {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //PSADSAINFpiaspofj
+       //paindiNDPINAIDP
+       System.out.println("Hello Pasquale Foggia!");
     }
     
 }

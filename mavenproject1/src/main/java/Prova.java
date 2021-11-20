44218d76ba146cdef7fd530b555252ee6c173524
Asus Pc <Asus Pc@LAPTOP-GA6ILVT2>
2021-11-20 17:01:15 +0100

    Aggiunta funzione hello

---
+++ mavenproject1/src/main/java/Prova.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author ary
+ */
+public class Prova {
+    public static void main(String args[]){
+        System.out.println("Hello, world");
+        System.out.println("Hello, universe");
+        System.out.println("Hello, universe");
+        
+    }
+    public static void salute(String whom){
+        System.out.println("Hello, "+ whom);
+    }
+}
